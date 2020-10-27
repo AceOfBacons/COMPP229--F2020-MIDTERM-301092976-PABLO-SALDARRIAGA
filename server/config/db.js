4565b90428403da8f463d8e4c0016aaff2673a1b
Pablo Saldarriaga <psaldarr@my.centennialcollege.ca>
2020-10-26 20:58:17 -0400

    Connected db to atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+    "URI": "mongodb+srv://pablo:Abysswalker123@dbserver.q6j71.mongodb.net/books229?retryWrites=true&w=majority"
 };
